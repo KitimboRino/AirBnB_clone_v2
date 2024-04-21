eaea76ed6ce1ff75042ea270e04f24dbc8595357
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2024-04-21 13:03:50 +0300

    fix: 0. Hello Flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Script that starts a Flask web application """
+""" Starts a Flask web application """
 
 from flask import Flask
 
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
-    """ Function that generates the main route """
+    """ Generates the main route """
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
