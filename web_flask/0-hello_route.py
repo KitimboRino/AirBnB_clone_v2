9474166e13aac363366be640121f763d0bacc644
Kitimbo Rino <kitimborinoemma0580@gmail.com>
2024-04-21 13:49:39 +0300

    refactor task one code styles

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,10 +5,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def index():
     """ Generates the main route """
     return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
